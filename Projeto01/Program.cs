574212cf5bb3db6ff175105c58e082f88f64b2c0
BrunoCampxs <bruno.andrade115@etec.gov.sp.br>
2024-10-08 21:54:26 -0300

    first commit

---
+++ Projeto01/Program.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlCommand cmd;
+            MySqlConnection conexao;
+            conexao = new MySqlConnection("server=localhost;Database=sapateiro;Uid=root;Pwd''");
+            try
+            {
+                conexao.Open();
+                Console.WriteLine("Conexao bem sucedida...");
+            }
+            catch (Exception ex)
+            { 
+            Console.WriteLine(ex.Message);
+            }
+            string sql;
+            sql = "select * from usuario";
+            cmd = new MySqlCommand(sql, conexao);
+            MySqlDataReader rdr;
+            rdr = cmd.ExecuteReader();
+
+            while (rdr.Read()) { 
+            Console.WriteLine("Id Usuario:" + rdr["id"] + "\n");
+                Console.WriteLine("Usuario" + rdr ["usuario"] + "\nSenha:" );
+            
+            }
+            Console.ReadKey();
+        }
+    }
+}
